71b59a692c5e39b3797988c255b5723fdd5bec23
poyoru0 <tky1091@icloud.com>
2026-06-19 18:48:52 +0900

    ウェブ基盤ほぼ完成
    レビューのスマホのタイトルが気になる

--- Scripts/Games/GamesSystem.js
+++ Scripts/Games/GamesSystem.js
@@ -14,7 +14,7 @@ fetch("/Scripts/Games/GamesData.json")
     //配列処理
     games.forEach(element => {
         //divの生成
-        const div = document.createElement("div");
+        const d = document.createElement("d");
 
         //タイトルの文字数取得
         const titleString = JSON.stringify(element.title);
@@ -28,23 +28,112 @@ fetch("/Scripts/Games/GamesData.json")
         else//スマホなら
         {
             fontsize = 24 - titleLength;
-            if(fontsize < 10)
+            if(fontsize < 5)
             {
-                fontsize = 10;
+                fontsize = 5;
             }
         }
-            console.log(gamesArea.style.fontSize)
         
         //表示内容
-        div.innerHTML = `
+        d.innerHTML = `
         <a href="${element.url}" target="_blank">
-            <img src="${element.image}">
-            <p style="font-size: ${fontsize + "px"}">${element.title}</p>
+            <div class="img">
+                <img src="${element.image}">
+            </div>
+                <p style="font-size: ${fontsize + "px"}">${element.title}</p>
         </a>
-        <button>レビューを書く</button>
+        <div class="button">
+            <button onclick="OpenReviewBtn('${element.title}')">レビューを書く</button>
+        </div>
         `;
 
         //サイトに表示
-        gamesArea.appendChild(div);
+        gamesArea.appendChild(d);
     });
 });
+
+//-----レビュー系-----
+
+const reviewcard = document.getElementById("reviewArea");
+reviewcard.style.visibility = "hidden";
+
+var reviewScore = 1;
+
+//レビュー画面を閉じるボタン
+function CloseReviewBtn()
+{
+    reviewcard.style.visibility = "hidden";
+}
+
+//レビューボタンを押すと押したゲームのタイトルが入ってレビューカードを出す
+function OpenReviewBtn(title)
+{
+    
+    const gameTitle = document.getElementById("reviewGameTitle");
+    gameTitle.textContent = "タイトル:" + title;
+
+    reviewcard.style.visibility = "visible";
+}
+
+//押したときに返されたscoreに応じて星の表示を変える
+function ReviwScore(score)
+{
+    //評価を保存しておく
+    reviewScore = score;
+
+    const stars = document.getElementById("stars");
+    const star1 = document.getElementById("star1");
+    const star2 = document.getElementById("star2");
+    const star3 = document.getElementById("star3");
+    const star4 = document.getElementById("star4");
+    const star5 = document.getElementById("star5");
+
+    /*いったん全て評価なしにする*/
+    star2.textContent = "☆"
+    star2.style.color = "rgb(127,127,127)";
+    star3.textContent = "☆"
+    star3.style.color = "rgb(127,127,127)";
+    star4.textContent = "☆"
+    star4.style.color = "rgb(127,127,127)";
+    star5.textContent = "☆"
+    star5.style.color = "rgb(127,127,127)";
+
+    //スコアに応じて星の表示を変える
+    switch(score)
+    {
+        case 5:
+            star5.textContent = "★"
+            star5.style.color = "yellow";
+        case 4:
+            star4.textContent = "★"
+            star4.style.color = "yellow";
+        case 3:
+            star3.textContent = "★"
+            star3.style.color = "yellow";
+        case 2:
+            star2.textContent = "★"
+            star2.style.color = "yellow";
+        case 1:
+            star1.textContent = "★"
+            star1.style.color = "yellow";
+            break;
+    }
+}
+
+async function SendReview()
+{
+    //送信する内容を取得
+    const gameTitle = document.getElementById("reviewGameTitle").textContent;
+    const score = reviewScore;
+    const comment = document.getElementById("reviewComment").value;
+
+    await fetch("https://shiny-sea-2a59.poyoroomweb.workers.dev",{
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            title: gameTitle,
+            review: score,
+            impression: comment
+        })
+    });
+}
